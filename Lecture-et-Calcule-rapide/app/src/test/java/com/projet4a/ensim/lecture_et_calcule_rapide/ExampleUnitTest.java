3f1cf584ef58a9216c563f4f8c0a8d2232bf16ab
Paul <paul.cordon@laposte.net>
2017-11-07 09:45:47 +0100

    empty project

---
+++ Lecture-et-Calcule-rapide/app/src/test/java/com/projet4a/ensim/lecture_et_calcule_rapide/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.projet4a.ensim.lecture_et_calcule_rapide;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
