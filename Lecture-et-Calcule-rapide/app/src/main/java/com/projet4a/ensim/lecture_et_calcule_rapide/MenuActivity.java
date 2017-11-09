ce6ed506df0cbe93d7dc202592028005017cdedf
Paul <paul.cordon@laposte.net>
2017-11-09 11:22:26 +0100

    création du menu et ouverture de MathExo1Activity

--- Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/MenuActivity.java
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/MenuActivity.java
@@ -1,7 +1,10 @@
 package com.projet4a.ensim.lecture_et_calcule_rapide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Button math=(Button) findViewById(R.id.BtnCalcul);
+        Button lecture=(Button)findViewById(R.id.BtnLecture);
+        math.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuActivity.this, MathExo1Activity.class));
+            }
+        });
+        lecture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
