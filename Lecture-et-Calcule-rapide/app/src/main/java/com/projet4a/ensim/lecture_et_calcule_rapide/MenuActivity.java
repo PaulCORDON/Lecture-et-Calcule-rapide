f0146eb44c09c51a88e54c55487e462f7c925e91
Paul <paul.cordon@laposte.net>
2017-11-09 10:32:38 +0100

    Ajout MenuActivity

--- Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/MenuActivity.java
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/MenuActivity.java
@@ -3,11 +3,11 @@ package com.projet4a.ensim.lecture_et_calcule_rapide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
     }
 }
