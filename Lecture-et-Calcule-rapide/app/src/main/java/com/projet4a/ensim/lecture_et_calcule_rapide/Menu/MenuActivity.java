0c53aed596f6028aec844c936ec470b0db5a2e2c
florentin72 <florentin.paillier@gmail.com>
2017-11-16 11:39:17 +0100

    Ajout de la partie lecture et modification du layout de parametrage calcul

--- Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/Menu/MenuActivity.java
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/Menu/MenuActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.projet4a.ensim.lecture_et_calcule_rapide.CalculRapide.Activity.MathExo1Activity;
+import com.projet4a.ensim.lecture_et_calcule_rapide.LectureRapide.Activity.LectureAccueilActivity;
 import com.projet4a.ensim.lecture_et_calcule_rapide.R;
 
 public class MenuActivity extends AppCompatActivity {
@@ -15,17 +16,24 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        /* creation des boutons maths et lecture*/
         Button math=(Button) findViewById(R.id.BtnCalcul);
         Button lecture=(Button)findViewById(R.id.BtnLecture);
+
+
+        /* si on clique sur math on va vers les exercice de maths*/
         math.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MenuActivity.this, MathExo1Activity.class));
             }
         });
+        /*si on clique sur lecture on va vers les exercices de lectures*/
         lecture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                startActivity(new Intent(MenuActivity.this, LectureAccueilActivity.class));
 
             }
         });
