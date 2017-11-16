1786a55aec3c28f633371fa38eabdba6abddb81d
Paul <paul.cordon@laposte.net>
2017-11-16 09:54:49 +0100

    Création de l'interface de ModifParamEm11Activity

---
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/ModifParamEm1Activity.java
@@ -0,0 +1,13 @@
+package com.projet4a.ensim.lecture_et_calcule_rapide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifParamEm1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modif_param_em1);
+    }
+}
