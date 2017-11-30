ae2c9ced566d0f9837df67ebcb3a783360572b00
grk <rocheteauguy@gmail.com>
2017-11-30 12:19:07 +0100

    modif_param_el1

---
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/LectureRapide/Activity/modif_param_el1.java
@@ -0,0 +1,15 @@
+package com.projet4a.ensim.lecture_et_calcule_rapide.LectureRapide.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.projet4a.ensim.lecture_et_calcule_rapide.R;
+
+public class modif_param_el1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modif_param_el1);
+    }
+}
