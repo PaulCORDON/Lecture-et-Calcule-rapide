a251ecbed664e0935c9c5f089866db196a1e35c8
grk <rocheteauguy@gmail.com>
2017-12-07 11:58:47 +0100

    Sauvegarde de la modif de ParamEl1

--- Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/LectureRapide/Model/ParamEl1.java
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/LectureRapide/Model/ParamEl1.java
@@ -8,32 +8,36 @@ import java.sql.Time;
 public class ParamEl1 extends ParamLecture{
 
     private int nbEnonce;
-    private Time tempsApparution;
+    private Long tempsApparution;
     private int nbApparution;
     private Boolean multipleApparution;
     private Boolean enonceDisparait;
 
 
-    ParamEl1(){
+    public ParamEl1(){}
 
+    public ParamEl1(int nbEnonce,Long tempsApparution, int nbApparution,Boolean multipleApparution,Boolean enonceDisparait ){
+        this.enonceDisparait = enonceDisparait;
+        this.multipleApparution = multipleApparution;
+        this.nbApparution = nbApparution;
+        this.tempsApparution = tempsApparution;
+        this.nbEnonce = nbEnonce;
 
     }
 
     //getters
     public int getNbEnonce() {return nbEnonce;}
-    public Time getTempsApparution() {return tempsApparution;}
+    public Long getTempsApparution() {return tempsApparution;}
     public int getNbApparution() {return nbApparution;}
     public Boolean getMultipleApparution() {return multipleApparution;}
     public Boolean getEnonceDisparait() {return enonceDisparait;}
 
     //setters
     public void setNbEnonce(int nbEnonce) {this.nbEnonce = nbEnonce;}
-    public void setTempsApparution(Time tempsApparution) {this.tempsApparution = tempsApparution;}
+    public void setTempsApparution(Long tempsApparution) {this.tempsApparution = tempsApparution;}
     public void setNbApparution(int nbApparution) {this.nbApparution = nbApparution;}
     public void setMultipleApparution(Boolean multipleApparution) {this.multipleApparution = multipleApparution;}
     public void setEnonceDisparait(Boolean enonceDisparait) {this.enonceDisparait = enonceDisparait;}
-
-
 
 
 }
