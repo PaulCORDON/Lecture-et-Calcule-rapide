e7aee2a9b373e45d3a0a6f9c28500bdcf5cf265a
Valentin LE MECHEC <valentin.lemechec@gmail.com>
2017-12-07 12:41:04 +0100

    presque fini la récupération des params

--- Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/ModifParamEm1Activity.java
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/ModifParamEm1Activity.java
@@ -24,12 +24,15 @@ public class ModifParamEm1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modif_param_em1);
-        SeekBar nbBornes= (SeekBar) findViewById(R.id.ChoixNbBornes);
-        SeekBar nbQuestions= (SeekBar) findViewById(R.id.ChoixNbQuestions);
 
-        RadioGroup group = (RadioGroup) findViewById(R.id.GroupRadiobutton);
-        RadioButton rb1=(RadioButton) findViewById(R.id.ChoixCalculAvantRep);
-        RadioButton rb2=(RadioButton) findViewById(R.id.ChoixRepAvantCalcul);
+
+
+        final SeekBar nbBornes=  findViewById(R.id.ChoixNbBornes);
+        final SeekBar nbQuestions=  findViewById(R.id.ChoixNbQuestions);
+
+
+        final RadioButton rb1= findViewById(R.id.ChoixCalculAvantRep);
+        final RadioButton rb2=findViewById(R.id.ChoixRepAvantCalcul);
         Boolean ordreApparition=true;
         if(rb1.isChecked()){
             ordreApparition=true;
@@ -37,42 +40,52 @@ public class ModifParamEm1Activity extends AppCompatActivity {
         if(rb2.isChecked()){
             ordreApparition=false;
         }
+        final Boolean finalOrdreApparition = ordreApparition;
 
-        Switch disparitionCalcul= (Switch) findViewById(R.id.ChoixDisparitionCalcul);
-        Switch bornesSelectionnables= (Switch) findViewById(R.id.ChoixBornesSelectionnables);
-        Switch bornesEgalesReps= (Switch) findViewById(R.id.ChoixBornesEgalesRep);
+        final Switch disparitionCalcul=  findViewById(R.id.ChoixDisparitionCalcul);
+        final Switch bornesSelectionnables=  findViewById(R.id.ChoixBornesSelectionnables);
+        final Switch bornesEgalesReps= findViewById(R.id.ChoixBornesEgalesRep);
 
-        EditText valMax= (EditText) findViewById(R.id.ValeurMax);
-        int valmax=Integer.parseInt(valMax.getText().toString());
+        final EditText valMax=  findViewById(R.id.ValeurMax);
+        //int valmax=Integer.parseInt(valMax.getText().toString());
 
-        EditText tpsReponse= (EditText) findViewById(R.id.tempsReponse);
-        Long tpsrep= Long.parseLong(tpsReponse.getText().toString());
+        final EditText tpsReponse=  findViewById(R.id.tempsReponse);
+       // Long tpsrep= Long.parseLong(tpsReponse.getText().toString());
 
-        Switch nbPairsOnly= (Switch) findViewById(R.id.ChoixNbPairs);
+        final Switch nbPairsOnly=  findViewById(R.id.ChoixNbPairs);
 
-        CheckBox addition= (CheckBox) findViewById(R.id.addition);
-        CheckBox soustraction= (CheckBox) findViewById(R.id.soustraction);
-        CheckBox division= (CheckBox) findViewById(R.id.division);
-        CheckBox multiplication= (CheckBox) findViewById(R.id.multiplication);
+        CheckBox addition=  findViewById(R.id.addition);
+        CheckBox soustraction=  findViewById(R.id.soustraction);
+        CheckBox division= findViewById(R.id.division);
+        CheckBox multiplication=  findViewById(R.id.multiplication);
 
-        Boolean[] operateurs= new Boolean[4];
+        final Boolean[] operateurs= new Boolean[4];
         operateurs[0]=addition.isChecked();
         operateurs[1]=soustraction.isChecked();
         operateurs[2]=multiplication.isChecked();
         operateurs[3]=division.isChecked();
 
 
-        Button valider= (Button) findViewById(R.id.BoutonValider);
-        final boolean enregistrer = valider.isPressed();
 
-        if(enregistrer){
-            //ligne suivante a revoir car les types renvoyés ne sont pas les types attendus
-            ParamEm1 param= new ParamEm1(tpsrep, nbPairsOnly.isChecked(), operateurs, nbBornes.getProgress(), nbQuestions.getProgress(),disparitionCalcul.isChecked(), ordreApparition, bornesSelectionnables.isChecked(),bornesEgalesReps.isChecked(),valmax);
+        Button valider=  findViewById(R.id.BoutonValider);
 
-            Log.d("DEBUG", "tps reponse: "+param.getTempsRep()+"\nnbs pairs seulement: "+param.getPairOnly()+"\noperateurs: "+param.getOperateur()+"\nnb bornes: "+param.getNbBornes()+
-            "\nnb questions: "+param.getNbQuestions()+"\ndisparition du calcul: "+param.getCalculDisparait()+"\nordre apparition: "+param.getOrdreApparition()+
-                    "\nbornes selectionnables: " +param.getBorneSelectionnable()+"\nbornes egales reponses: "+param.getBorneEqualsOp()+"\nvaleur max: "+param.getValMax());
-        }
+
+        valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                ParamEm1 param= new ParamEm1(Long.parseLong(tpsReponse.getText().toString()), nbPairsOnly.isChecked(), operateurs, nbBornes.getProgress(), nbQuestions.getProgress(),disparitionCalcul.isChecked(), finalOrdreApparition, bornesSelectionnables.isChecked(),bornesEgalesReps.isChecked(),Integer.parseInt(valMax.getText().toString()));
+
+                Log.i("info", "tps reponse: "+param.getTempsRep()+"\nnbs pairs seulement: "+param.getPairOnly()+"\noperateurs: "+param.getOperateur()+"\nnb bornes: "+param.getNbBornes()+
+                        "\nnb questions: "+param.getNbQuestions()+"\ndisparition du calcul: "+param.getCalculDisparait()+"\nordre apparition: "+param.getOrdreApparition()+
+                        "\nbornes selectionnables: " +param.getBorneSelectionnable()+"\nbornes egales reponses: "+param.getBorneEqualsOp()+"\nvaleur max: "+param.getValMax());
+
+            }
+
+        });
+
+
 
 
 
