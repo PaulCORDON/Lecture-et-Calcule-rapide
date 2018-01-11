0b32a32a95155cfd24b1cb85d39a99c921b436c1
Kilian sanchez <kilian0925@gmail.com>
2018-01-11 10:58:33 +0100

    affichage du résultat v1

--- Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/ExoMath1Resultat.java
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/ExoMath1Resultat.java
@@ -1,15 +1,44 @@
 package com.projet4a.ensim.lecture_et_calcule_rapide.CalculRapide.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.projet4a.ensim.lecture_et_calcule_rapide.R;
 
+import java.util.logging.Logger;
+
 public class ExoMath1Resultat extends AppCompatActivity {
 
+    /**
+     * tableau des reponses de l'élève
+     */
+    private static boolean[] reponseJuste;
+
+    private int bonneRep;
+    //coucou
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exo_math1_resultat);
+        Intent intent = getIntent();
+
+        // on récupere le tableau contenant les réponses données par l'élève
+        reponseJuste = intent.getBooleanArrayExtra("ReponseDonnee");
+
+
+        TextView nbRep = (TextView) findViewById(R.id.score);
+
+        for(boolean b : reponseJuste){
+            if(b){
+                Log.i("reponse juste","rep" + b);
+                bonneRep++;
+            }
+        }
+
+        nbRep.setText(""+bonneRep);
+
     }
 }
