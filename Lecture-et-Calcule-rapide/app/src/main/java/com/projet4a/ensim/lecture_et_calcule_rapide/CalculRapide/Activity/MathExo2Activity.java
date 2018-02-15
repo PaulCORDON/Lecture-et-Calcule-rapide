5720801f2373ee58160383461ed3d7bccb6dfba5
Kilian sanchez <kilian0925@gmail.com>
2018-02-15 10:00:14 +0100

    Ajout activity exo2 math

--- Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/MathExo2Activity.java
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/MathExo2Activity.java
@@ -3,6 +3,14 @@ package com.projet4a.ensim.lecture_et_calcule_rapide.CalculRapide.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.projet4a.ensim.lecture_et_calcule_rapide.R;
+
+
+/**
+ * Exercice deux de maths,
+ * Exercice de multiplication
+ */
+
 public class MathExo2Activity extends AppCompatActivity {
 
     @Override
