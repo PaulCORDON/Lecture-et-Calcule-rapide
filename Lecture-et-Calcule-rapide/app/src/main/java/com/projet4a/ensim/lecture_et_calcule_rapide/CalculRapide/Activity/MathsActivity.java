f71c6aef91b5dabb205ff45c2d10b3b61c3a2d32
Paul <paul.cordon@laposte.net>
2018-02-19 13:59:15 +0100

    Amélioration des menus de math et de lecture

--- Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/MathsActivity.java
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/MathsActivity.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 
 import com.projet4a.ensim.lecture_et_calcule_rapide.R;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class MathsActivity extends AppCompatActivity {
 
 /* booleens pour savoir quel exercice est selectionné */
@@ -24,16 +26,22 @@ public class MathsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maths);
 
-    /* creation des boutons exercice 1,2,3 qui serviront pour le calcul et la lecture*/
+        /* creation des boutons exercice 1,2,3 qui serviront pour le calcul et la lecture*/
         final Button exercice1 = findViewById(R.id.exo1M);
         final Button exercice2 = (Button) findViewById(R.id.exo2M);
         final Button exercice3 = (Button) findViewById(R.id.exo3M);
-    /* creation du boutton parametre */
+        /* creation du boutton parametre */
         final Button parametreM = (Button) findViewById(R.id.paramM);
-    /*creation bouton goM */
+        /*creation bouton goM */
+        final Button go = findViewById(R.id.goM);
+        /* creation de la text view de description */
+        final TextView descriptionM = findViewById(R.id.descriptionM);
 
-    final  Button go = findViewById(R.id.goM);
+        final GifImageView gifExo = findViewById(R.id.Gif);
 
+        gifExo.setVisibility(View.GONE);
+        parametreM.setVisibility(View.GONE);
+        go.setVisibility(View.GONE);
 
 
         /** click sur le bouton exercice 1 , on affiche la description et on met le bouton enable */
@@ -43,13 +51,14 @@ public class MathsActivity extends AppCompatActivity {
                 exercice1.setEnabled(false);
                 exercice2.setEnabled(true);
                 exercice3.setEnabled(true);
+                parametreM.setVisibility(View.VISIBLE);
+                go.setVisibility(View.VISIBLE);
+                gifExo.setVisibility(View.VISIBLE);
                 isExercice1 = true;
                 isExercice2 = false;
                 isExercice3 = false;
-                descriptionM = (TextView) findViewById(R.id.descriptionM);
 
-
-                descriptionM.setText("Exercice 1 de Maths !!!!! ");
+                descriptionM.setText("Exercice 1 de Mathématiques\nConsigne : Trouve où se situe le résultat du calcule");
 
 
 
@@ -63,13 +72,14 @@ public class MathsActivity extends AppCompatActivity {
                 exercice1.setEnabled(true);
                 exercice2.setEnabled(false);
                 exercice3.setEnabled(true);
+                parametreM.setVisibility(View.VISIBLE);
+                go.setVisibility(View.VISIBLE);
+                gifExo.setVisibility(View.VISIBLE);
                 isExercice1 = false;
                 isExercice2 = true;
                 isExercice3 = false;
-                descriptionM = (TextView) findViewById(R.id.descriptionM);
 
-
-                descriptionM.setText("Exercice 2 de Maths !!!!! ");
+                descriptionM.setText("Exercice 2 de Mathématiques\nConsigne : ");
 
 
 
@@ -82,13 +92,14 @@ public class MathsActivity extends AppCompatActivity {
                 exercice1.setEnabled(true);
                 exercice2.setEnabled(true);
                 exercice3.setEnabled(false);
+                parametreM.setVisibility(View.VISIBLE);
+                go.setVisibility(View.VISIBLE);
+                gifExo.setVisibility(View.VISIBLE);
                 isExercice1 = false;
                 isExercice2 = false;
                 isExercice3 = true;
-                descriptionM = (TextView) findViewById(R.id.descriptionM);
 
-
-                descriptionM.setText("Exercice 3 de Maths !!!!! ");
+                descriptionM.setText("Exercice 3 de Mathématiques\nConsigne : ");
 
 
             }
