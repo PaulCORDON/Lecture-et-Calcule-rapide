d701da3db2cbee249bbfc5fca2c897d3c27c1d67
guy <mgrkrr21@gmail.com>
2018-03-01 10:45:17 +0100

    param calcul exo2

--- Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/ModifParamEm2Activity.java
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/ModifParamEm2Activity.java
@@ -1,14 +1,16 @@
 package com.projet4a.ensim.lecture_et_calcule_rapide.CalculRapide.Activity;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
+import com.projet4a.ensim.lecture_et_calcule_rapide.CalculRapide.Model.ParamEm2;
 import com.projet4a.ensim.lecture_et_calcule_rapide.R;
 
 /**
@@ -45,11 +47,108 @@ public class ModifParamEm2Activity extends AppCompatActivity {
 
                 if (!nbCal.getText().toString().equals(""))
                     nbcal = Integer.parseInt(nbCal.getText().toString());
-                else nbcal = 5;
+                else nbcal = 10;
 
                 if (!valeurMax.getText().toString().equals(""))
                     valMax = Integer.parseInt(valeurMax.getText().toString());
-                else valMax = 30;
+                else valMax = 10;
+
+                boolean npair=false, nimpair=false,repdeuxbrnes=false,repQuatrebrnes=false,pav=false;
+                int typerep = 0;
+
+                if(nbpair.isChecked()){
+                    npair=true;
+                }
+
+                if(nbpair.isChecked()){
+                    nimpair=true;
+                }
+                if(deuxbornes.isChecked()){
+                    repdeuxbrnes = true;
+                    typerep=1;
+                }else
+                if(quatrebornes.isChecked()){
+                    repQuatrebrnes = true;
+                    typerep=2;
+                }else
+                if(pavNum.isChecked()){
+                    pav = true;
+                    typerep=0;
+                }
+
+                ParamEm2 param = null;
+
+                /** Si les deux champs de saisie pour le nombre de calcul et la valeur max des opérandes sont remplis on recupere les valeurs des parametres
+                 * et on construit un nouvel objet param avec ces parametres
+                 */
+                if(!valeurMax.getText().toString().equals("") && !nbCal.getText().toString().equals("")) {
+                    param = new ParamEm2(
+                            typerep,
+                            nbcal,
+                            valMax,
+                            nimpair,
+                            npair,
+                            repdeuxbrnes,
+                            pav,
+                            repQuatrebrnes);
+                    Log.i("info", "type de reponse:"+param.gettypeRep()+"nb calcul: " + param.getNbCalcul() + "\nvaleur max des operandes: " + param.getValMaxOperande() + "\nnombre pair: " + param.getNombrePair() + "\nnb impair: " + param.getNombreImpair() +
+                            "\ndeux bornes: " + param.getRepDeuxBornes() + "\npave num: " + param.getRepPaveNum() + "\nquatre bornes: " + param.getRepQuatreBornes());
+                }
+                /** Si le champ de saisie pour le nombre de calcul et la valeur max des opérandes ne sont pas remplis on recupere les valeurs des parametres
+                 * et on construit un nouvel objet param avec ces parametres
+                 */
+                if(valeurMax.getText().toString().equals("") && nbCal.getText().toString().equals("")) {
+                    param = new ParamEm2(
+                            typerep,
+                            10,
+                            10,
+                            nimpair,
+                            npair,
+                            repdeuxbrnes,
+                            pav,
+                            repQuatrebrnes);
+                    Log.i("info", "type de reponse:"+param.gettypeRep()+"nb calcul: " + param.getNbCalcul() + "\nvaleur max des operandes: " + param.getValMaxOperande() + "\nnombre pair: " + param.getNombrePair() + "\nnb impair: " + param.getNombreImpair() +
+                            "\ndeux bornes: " + param.getRepDeuxBornes() + "\npave num: " + param.getRepPaveNum() + "\nquatre bornes: " + param.getRepQuatreBornes());
+                }
+                /** Si le champ de saisie pour le nombre de calcul est rempli on recupere les valeurs des parametres
+                 * et on construit un nouvel objet param avec ces parametres
+                 */
+                if(valeurMax.getText().toString().equals("") && !nbCal.getText().toString().equals("")) {
+                    param = new ParamEm2(
+                            typerep,
+                            nbcal,
+                            10,
+                            nimpair,
+                            npair,
+                            repdeuxbrnes,
+                            pav,
+                            repQuatrebrnes);
+                    Log.i("info", "type de reponse:"+param.gettypeRep()+"nb calcul: " + param.getNbCalcul() + "\nvaleur max des operandes: " + param.getValMaxOperande() + "\nnombre pair: " + param.getNombrePair() + "\nnb impair: " + param.getNombreImpair() +
+                            "\ndeux bornes: " + param.getRepDeuxBornes() + "\npave num: " + param.getRepPaveNum() + "\nquatre bornes: " + param.getRepQuatreBornes());
+                }
+                /** Si le champ de saisie pour la valeur max des operandes est rempli on recupere les valeurs des parametres
+                 * et on construit un nouvel objet param avec ces parametres
+                 */
+                if(!valeurMax.getText().toString().equals("") && nbCal.getText().toString().equals("")) {
+                    param = new ParamEm2(
+                            typerep,
+                            10,
+                            valMax,
+                            nimpair,
+                            npair,
+                            repdeuxbrnes,
+                            pav,
+                            repQuatrebrnes);
+                    Log.i("info", "type de reponse:"+param.gettypeRep()+"nb calcul: " + param.getNbCalcul() + "\nvaleur max des operandes: " + param.getValMaxOperande() + "\nnombre pair: " + param.getNombrePair() + "\nnb impair: " + param.getNombreImpair() +
+                            "\ndeux bornes: " + param.getRepDeuxBornes() + "\npave num: " + param.getRepPaveNum() + "\nquatre bornes: " + param.getRepQuatreBornes());
+                }
+
+
+
+                /** quand on a clique sur le bouton valider on reviens au menu*/
+                Intent intent=new Intent(ModifParamEm2Activity.this, MathsActivity.class);
+                startActivity(intent);
+
             }
 
         });
