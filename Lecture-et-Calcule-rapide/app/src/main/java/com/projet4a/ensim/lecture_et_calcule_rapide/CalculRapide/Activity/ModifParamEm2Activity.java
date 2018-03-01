b6aae5abf76f85f459e408d7d4291d6b32ee2b90
guy <mgrkrr21@gmail.com>
2018-03-01 12:05:03 +0100

    commentaire math param exo 2

--- Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/ModifParamEm2Activity.java
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/CalculRapide/Activity/ModifParamEm2Activity.java
@@ -1,5 +1,6 @@
 package com.projet4a.ensim.lecture_et_calcule_rapide.CalculRapide.Activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,9 @@ import android.widget.RadioButton;
 import com.projet4a.ensim.lecture_et_calcule_rapide.CalculRapide.Model.ParamEm2;
 import com.projet4a.ensim.lecture_et_calcule_rapide.R;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * Created by HP on 22/02/2018.
  */
@@ -23,6 +27,10 @@ public class ModifParamEm2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modif_param_em2);
+        /**
+         *
+         * recupération des différents id de activity_modif_paramEm2.xml
+         */
 
         final EditText  nbCal=  findViewById(R.id.NbCalcul);
         final EditText  valeurMax=  findViewById(R.id.ValMaxOperandes);
@@ -37,6 +45,9 @@ public class ModifParamEm2Activity extends AppCompatActivity {
         /** Bouton qui permet de valider les parametres et de retourner sur la page d'accueil*/
         Button valider=  findViewById(R.id.BtonValider);
 
+        /**
+         * si on clique sur le bouton valider
+         */
         valider.setOnClickListener(new View.OnClickListener() {
             @Override
             /** si le bouton valider a ete cliqué on rentre dans cette methode*/
@@ -45,6 +56,9 @@ public class ModifParamEm2Activity extends AppCompatActivity {
                 int nbcal;
                 int valMax;
 
+                /**
+                 * verifier si les valeurs de nombre max de calcul et des opérandes sont saisies
+                 */
                 if (!nbCal.getText().toString().equals(""))
                     nbcal = Integer.parseInt(nbCal.getText().toString());
                 else nbcal = 10;
@@ -53,9 +67,15 @@ public class ModifParamEm2Activity extends AppCompatActivity {
                     valMax = Integer.parseInt(valeurMax.getText().toString());
                 else valMax = 10;
 
+                /**
+                 * declaration des variables
+                 */
                 boolean npair=false, nimpair=false,repdeuxbrnes=false,repQuatrebrnes=false,pav=false;
                 int typerep = 0;
 
+                /**
+                 * récupération des valeurs entrées par l'utilisateur
+                 */
                 if(nbpair.isChecked()){
                     npair=true;
                 }
@@ -144,6 +164,18 @@ public class ModifParamEm2Activity extends AppCompatActivity {
                 }
 
 
+                FileOutputStream outputStream;
+                ObjectOutputStream oos;
+                try {
+                    outputStream = openFileOutput("ParamEm2.txt", Context.MODE_PRIVATE);
+                    oos = new ObjectOutputStream(outputStream);
+                    oos.writeObject(param);
+
+                    oos.flush();
+                    oos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
                 /** quand on a clique sur le bouton valider on reviens au menu*/
                 Intent intent=new Intent(ModifParamEm2Activity.this, MathsActivity.class);
