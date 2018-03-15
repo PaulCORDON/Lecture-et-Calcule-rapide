a93fe224a8a3ab83b0dd21a2b5c2307d3c16d054
Paul <paul.cordon@laposte.net>
2018-03-15 12:18:11 +0100

    Test QrCODE

---
+++ Lecture-et-Calcule-rapide/app/src/main/java/com/projet4a/ensim/lecture_et_calcule_rapide/EnvoiResultat/GenerateQRCodeActivity.java
@@ -0,0 +1,74 @@
+package com.projet4a.ensim.lecture_et_calcule_rapide.EnvoiResultat;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.projet4a.ensim.lecture_et_calcule_rapide.Menu.MenuActivity;
+import com.projet4a.ensim.lecture_et_calcule_rapide.R;
+
+
+
+
+public class GenerateQRCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_qrcode);
+
+        /*
+        lien avec la vue
+         */
+        final ImageView qrCode = (ImageView) findViewById(R.id.QRCode);
+        final Button retourMenu=(Button) findViewById(R.id.BtnMenu);
+        final Button generer=(Button)findViewById(R.id.BtnGenerer);
+        final EditText nom=(EditText) findViewById(R.id.Nom);
+        final EditText prenom=(EditText) findViewById(R.id.Prenom);
+
+
+        generer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message=nom.getText()+" "+prenom.getText()+" "+getIntent().getIntegerArrayListExtra("bonneRep")+" "+getIntent().getIntegerArrayListExtra("nbRep");
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try{
+                    BitMatrix bitMatrix=multiFormatWriter.encode(message, BarcodeFormat.QR_CODE,200,200);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap=barcodeEncoder.createBitmap(bitMatrix);
+                    qrCode.setImageBitmap(bitmap);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+
+        /*
+        Si on clic sur le bouton "retour vers le menu" on retourne au menu
+         */
+        retourMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GenerateQRCodeActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    }
+}
